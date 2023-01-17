5bda42f9b3684debbf6e8ed608864c06e01e6cc7
Guillermo Figueroa Mesa <gfirem@gmail.com>
2023-01-17 17:39:44 -0300

    * Updated the stable version from the readme.txt + added the laravel mix setup and base files

--- sca-recipe.php
+++ sca-recipe.php
@@ -7,7 +7,7 @@
  * Requires at least: 4.6
  * Tested up to: 6.1.1
  * Requires PHP: 7.4
- * Stable tag: 1.0.1
+ * Stable tag: 1.0.0
  * Author: Scalater Team
  * License: GPLv2 or later
  * Network: false
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,20 +1,12 @@
 const mix = require('laravel-mix');
-const assetsPath = 'src';
+const assetsPath = 'assets';
 const distributionPath = 'assets';
 
 mix.setPublicPath(`${distributionPath}`);
-mix.sass(`${assetsPath}/styles/forms.scss`, `${distributionPath}/css`).sourceMaps();
-mix.sass(`${assetsPath}/styles/filters.scss`, `${distributionPath}/css`).sourceMaps();
-mix.js(`${assetsPath}/scripts/forms.js`, `${distributionPath}/js`);
-mix.js(`${assetsPath}/scripts/filters.js`, `${distributionPath}/js`);
-mix.js(`${assetsPath}/scripts/lemonlight-page-pillar-content.js`, `${distributionPath}/js`).react();
-mix.js(`${assetsPath}/scripts/lemonlight-post-sidebar-options.js`, `${distributionPath}/js`).react();
+mix.sass(`${assetsPath}/scss/styles.scss`, `${distributionPath}/css`).sourceMaps();
+mix.js(`${assetsPath}/js/script.js`, `${distributionPath}/js`);
 
 mix.minify([
-  `${distributionPath}/css/forms.css`,
-  `${distributionPath}/css/filters.css`,
-  `${distributionPath}/js/forms.js`,
-  `${distributionPath}/js/filters.js`,
-  `${distributionPath}/js/lemonlight-page-pillar-content.js`,
-  `${distributionPath}/js/gutenberg/lemonlight-page-pillar-content-metafield.js`,
+  `${distributionPath}/css/styles.css`,
+  `${distributionPath}/js/script.js`,
 ]);
